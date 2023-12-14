f51e2860b3b6afd894aeca2275dc39863f8a3b36
Sahal Palayat <sahalpalayat5894@gmail.com>
2023-12-14 21:47:16 +0530

    user

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -1,16 +1,38 @@
-const isLogin = async(req,res,next)=>{
-    try {
-       if(req.session.user_id){
-          next()
-       }
-       else{
-          res.redirect('/login');
-       }
-    } catch (error) {
-       console.log(error.message)
-    }
- }
+
   
+const {User}=require('../models/schemas')
+
+const isLogin = async (req, res, next) => {
+  try {
+    if (req.session.user_id) {
+      const user = await User.findById(req.session.user_id);
+
+      if (user) {
+        if (user.is_blocked) {
+          req.session.destroy((err) => {
+            if (err) {
+              console.error(err);
+              res.status(500).send('Internal Server Error');
+            } else {
+              res.redirect('/login');
+            }
+          });
+        } else {
+          next();
+        }
+      } else {
+        res.redirect('/login');
+      }
+    } else {
+      res.redirect('/login');
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+
  const isLogout=async (req,res,next)=>{
     try {
         if(req.session.user_id){
